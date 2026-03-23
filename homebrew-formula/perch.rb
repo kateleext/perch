beb7b13ef251a85b02d16344ff4db3de30a6808a
kate <kate@takuma.ai>
2026-03-23 15:48:00 -0700

    Update homebrew formula to v0.0.6

--- homebrew-formula/perch.rb
+++ homebrew-formula/perch.rb
@@ -1,28 +1,28 @@
 class Perch < Formula
   desc "Minimal file viewer for coding agents"
   homepage "https://github.com/kateleext/perch"
-  version "0.0.4"
+  version "0.0.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kateleext/perch/releases/download/v0.0.4/perch_darwin_arm64.tar.gz"
-      sha256 "0281a807d8a8d4334a2bd9d69634a12863e19b46639ecfb39a70fbff328793ba"
+      url "https://github.com/kateleext/perch/releases/download/v0.0.6/perch_darwin_arm64.tar.gz"
+      sha256 "3c1e9628d72c2a456f10cb8ab319006ced20ef40231244f7caf02ce259765032"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kateleext/perch/releases/download/v0.0.4/perch_darwin_amd64.tar.gz"
-      sha256 "8f196ea5795ca95f5f273e10110be8be222089ccfcaa59060794bee50b3f9d35"
+      url "https://github.com/kateleext/perch/releases/download/v0.0.6/perch_darwin_amd64.tar.gz"
+      sha256 "d6f4076f367480157302545c4284f4ccf3ba5333f50ac8bd807474ef448ee2ad"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kateleext/perch/releases/download/v0.0.4/perch_linux_arm64.tar.gz"
-      sha256 "21593a52d2bd4d8d692d7b8d06ef4403c18f515c22612e115ed9d9c5fb2c7b1b"
+      url "https://github.com/kateleext/perch/releases/download/v0.0.6/perch_linux_arm64.tar.gz"
+      sha256 "293e81509878f7f381dd5af1bc2b55939db297378968072f060f8461434a15cb"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kateleext/perch/releases/download/v0.0.4/perch_linux_amd64.tar.gz"
-      sha256 "221a8e69ca4ecdbe5a90b344907b9a706092c587da863126d3cbe9d2dc710bdd"
+      url "https://github.com/kateleext/perch/releases/download/v0.0.6/perch_linux_amd64.tar.gz"
+      sha256 "1fd8ba52332b3c6ef95252211cacdc48d0c174ac8c73bd468a12362e20b671d8"
     end
   end
 
